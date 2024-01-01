43a7f77d2e0ac9ba92d204a627d27fdb281ea236
Yongye Tan <yongyetan1209@gmail.com>
2024-01-01 01:46:31 -0500

    Refactor

--- client/src/routes/App.svelte
+++ client/src/routes/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Navbar from "$components/Navigation/Navbar.svelte";
+  import Navbar from "./Navbar.svelte";
   import CreateEvent from "$components/Event/CreateEvent.svelte";
   import EventCards from "$components/Event/EventCards.svelte";
 </script>
